fd799ced2a8a25111faafa74546d72b51090fd58
jimealcuaz <alcuazjimena@gmail.com>
2023-01-15 14:05:23 -0300

    probando carrito

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ let seguroTercero = document.querySelector("#segurotercero")
 let fechaSiniestro = document.querySelector("#fechasiniestro")
 let lugarSiniestro = document.querySelector("#lugarsiniestro")
 let formularioTitular = document.querySelector("#form-titular")
-const reclamo = [];
 
-    formularioTitular.addEventListener("submit", (e)=>{
+const reclamo = []
+
+formularioTitular.addEventListener("submit", (e) => {
     e.preventDefault()
     formularioTitular.innerHTML = `
     <div class="alert alert-info" role="alert">
@@ -16,7 +17,6 @@ const reclamo = [];
         <p>Gracias ${reclamante.value} por cargar los datos del siniestro.</p>
     </div> 
     `
-    })
 
     const reclamoCargado = {
         reclamante: `${reclamante.value}`,
@@ -28,6 +28,49 @@ const reclamo = [];
         lugarSiniestro: `${lugarSiniestro.value}`,
         formularioTitular: `${formularioTitular.value}`,
     }
+
     reclamo.push(reclamoCargado)
     const aJson = JSON.stringify(reclamo)
     localStorage.setItem("reclamos", aJson)
+})
+
+const servicios = [
+    { nombre: "Familia", precio: 1000, id: 1 },
+    { nombre: "Civil", precio: 2000, id: 2 },
+    { nombre: "Laboral", precio: 1000, id: 3 },
+    { nombre: "Penal", precio: 3000, id: 4 },
+]
+
+const botonAgregar = document.querySelectorAll(".btn-agregar")
+addButton.forEach(el => {
+    el.addEventListener("click", (e) => {
+        agregarAlCarrito(e.target.id)
+    })
+});
+
+const carrito = []
+
+const agregarAlCarrito = (id) => {
+    let nuevoServicio = servicios.find(p => p.id === parseInt(id))
+    carrito.push(nuevoServicio)
+    cargarCarrito(carrito)
+    calcularTotalCompra()
+} 
+
+const tablaCarrito = (c)=> {
+    return `<tr>
+    <td class="elemento">${c.id} -</td>
+    <td class="elemento">${c.nombre}</td>
+    <td class="elemento">${c.precio}</td>
+</tr>`
+}
+const cargarCarrito = (carrito)=> {
+    let compra = ""
+    if (carrito.length > 0){
+        carrito.forEach(c => {
+            compra += tablaCarrito(c)
+        })
+        tablaCarrito.innerHTML = compra
+    }
+}
+
